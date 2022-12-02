06a24de618c33a3c09512f0b6c0655ccd76770c3
hakan_dinc <119066406+hakandinc61@users.noreply.github.com>
2022-12-02 21:57:33 +0300

    Add files via upload

---
+++ tc_dogrulama.py
@@ -0,0 +1,21 @@
+def Tc(value=22205648594):
+    value=str(value)
+
+    if not len(value)==11:
+        return False
+
+    if not value.isdigit():
+        return False
+
+    if int(value[0])==0:
+        return False
+
+    digits=[int(d) for d in str(value)]
+
+    if not sum(digits[:10])%10==digits[10]:
+        return False
+
+    if not(((7*sum(digits[:9][-1::-2]))-sum(digits[:9][-2::-2]))%10)==digits[9]:
+        return False
+
+    return True
